fe6ac3e0b7ba58c0b7a80b1fe5b713a9c5491435
exnnwc <2buzezsn@gmail.com>
2016-03-10 13:47:18 -0800

    Setting up the board

---
+++ Board.php
@@ -0,0 +1,28 @@
+<?php
+
+class Board{
+    public $visible;
+    public $bombs;
+    function __construct($size){
+        for($x=0;$x<$size;$x++){
+            for($y=0;$y<$size;$y++){
+                $this->visibile[$x][$y]=false;
+            }   
+        }
+        $this->bombs=$this->populate_with_bombs($size, 10);
+    }
+    private function populate_with_bombs($size, $percent){
+        $bombs=[];
+        $num_of_bombs=0;
+        $max_num_of_bombs=$size*$size*(1/$percent);
+        while ($num_of_bombs<$max_num_of_bombs){
+            $rand_x=rand(0,$size-1);
+            $rand_y=rand(0, $size-1);
+                if (!isset($bombs[$rand_x][$rand_y])){
+                    $bombs[$rand_x][$rand_y]=true;
+                    $num_of_bombs++; 
+                }
+        }
+        return $bombs;
+    }
+}
--- index.php
+++ index.php
@@ -1,2 +1,47 @@
 <?php
+require_once("Board.php");
+define("BOARD_SIZE", 25);
+define("BOMB_PERCENT", 10);
 
+?>
+<html>
+<head>
+    <style>
+        table, tr, td{
+            border:1px black solid;
+            text-align:center;
+        }
+        td{
+            width:25px;
+            height:25px;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+
+<?php
+$board = new Board(BOARD_SIZE);
+
+
+for ($y = 0; $y<BOARD_SIZE; $y++){
+    echo "<tr>";
+    for ($x=0; $x<BOARD_SIZE; $x++){
+        echo "<td";
+        if (!$board->visible){
+            echo " style='background-color:black'>";
+        } else if (isset($board->bombs[$x][$y])){
+            echo ">x"; 
+        }
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+
+?>
+</table>
+</body>
+</html>
+
+<?php
